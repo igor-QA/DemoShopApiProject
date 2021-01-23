92425124cc64198db28c0285e296ea625161b6e8
Igor Pavlov <igortvk@ya.ru>
2021-01-23 17:19:50 +0300

    refactoring

--- src/test/java/AuthApi.java
+++ src/test/java/AuthApi.java
@@ -1,36 +1,45 @@
 import io.qameta.allure.Step;
 import io.qameta.allure.restassured.AllureRestAssured;
-import io.restassured.http.ContentType;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
-public class AuthApi {
+public class AuthApi{
     private final static String EMAIL = "test123@ya.ru";
     private final static String PASSWORD = "testtest13";
-    private final static String COOKIE_REQUEST = "ARRAffinity=7f10010dd6b12d83d6aefe199065b2e8fe0d0850a7df2983b482815225e42439; __utmc=78382081; __utmz=78382081.1610199709.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __RequestVerificationToken=4jQhs8OVpLKvZIAiaE9ngYgXQ25WraOZljuhYUm19-MECqCGVrpggMsP9a8B7Ky2_lKtQryXH480Xzt8OJq5AhNajRFviqSzLVRyXlMVrWQ1; ASP.NET_SessionId=v0o024f4vrgupcc5zcl01hww; __utma=78382081.517907423.1610199709.1610294821.1610363192.4; __utmt=1; Nop.customer=55f5107c-b195-4def-b739-a6945fe745d3; __utmb=78382081.6.10.1610363192";
+
 
     @Step("Get new session Cookie")
     public Map<String, String> cookies() {
 
-        Map<String, String> cookies = given()
+        return given()
                 .filter(new AllureRestAssured())  //.filter(filters().withCustomTemplates()) Для логов
-                .accept(ContentType.ANY) //Принимаем любой контент
+                //.accept(ContentType.ANY) //Принимаем любой контент
                 .contentType("application/x-www-form-urlencoded")
-                .cookie(COOKIE_REQUEST)
-                .body(data())
-                .basePath("/login")
+
+                .body(getData())
+                //.basePath("/login")
         .when()
-                .post()
+                .post("/login")
         .then()
                 .log().all()
                 .statusCode(302)
                 .extract().cookies();
 
-        return cookies;
+
     }
 
-    private String data() {
-        return "email=" + EMAIL + "&password=" + PASSWORD;
+    private String getData() {
+        return "email=" + EMAIL + "&password=" + PASSWORD; //потому что = application/x-www-form-urlencoded
     }
 }
+
+
+
+
+
+
+
+
+
+
--- src/test/java/DemoShopTests.java
+++ src/test/java/DemoShopTests.java
@@ -2,16 +2,13 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 @Owner("Igor Pavlov")
 @Feature("Login via API")
-@Tag("api")
 
 public class DemoShopTests {
 
@@ -22,16 +19,15 @@ public class DemoShopTests {
 
     @Test
     void openUrlWithAuthCookies() {
-
         //Передаем куки для авторизации
-        open("http://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/logo.png");
         AuthApi api = new AuthApi();
+        open("http://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/logo.png");
         CookieManager cookieManager = new CookieManager(api.cookies());
         cookieManager.addCookiesToSite();
 
         //Делаем дейсвия на сайте уже авторизованные
         open("http://demowebshop.tricentis.com");
-        $(By.className("account")).click();
+        $(".account").click();
         $("div.listbox ul li:nth-child(2) a").click();
         $("div.add-button").click();
         $("h1").shouldHave(text("My account - Add new address"));
